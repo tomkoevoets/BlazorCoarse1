fb05af0ae9ba046fa31e4447fd1797335fc6ecbe
tomkoevoets <tomkoevoets1@gmail.com>
2025-04-09 15:31:58 +0200

    Lesson 49

--- ServerManagement/Models/Todo.cs
+++ ServerManagement/Models/Todo.cs
@@ -7,7 +7,19 @@ namespace ServerManagement.Models
         public int Id { get; set; }
         public string Name { get; set; } = "";
 
-        public bool IsCompleted { get; set; }
+        public bool _IsCompleted { get; set; }
+        public bool IsCompleted 
+        { 
+            get => _IsCompleted;
+            set
+            {
+                _IsCompleted = value;
+                if (value)
+                {
+                    DateCompleted = DateTime.Now;
+                }
+            }
+        }
 
         public DateTime DateCompleted { get; set; }
     }
